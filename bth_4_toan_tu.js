74a4d7e287b9339d500c7565fb7be7820efdb8a4
thanhbui186 <thanh1862001@gmail.com>
2025-06-17 23:24:51 +0700

    Initialcommit

---
+++ bth_4_toan_tu.js
@@ -0,0 +1,12 @@
+let inputWidth
+let inputHeight
+
+inputWidth = prompt('Nhập vào chiều rộng :')
+inputHeight = prompt('Nhập vào chiều cao :')
+
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+
+let area = width * height
+
+document.write('Diện tích hình chữ nhật là : ' + area)
